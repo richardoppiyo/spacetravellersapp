1a9c70965b8a19070933c338ef7cc7d050e5d32b
ivaan1992 <ivaan.lg.92@gmail.com>
2022-04-05 11:06:24 -0500

    Call the ROCKET API

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Rockets = () => (
-  <div>
-    <h1>Rockets section</h1>
-  </div>
-);
+const Rockets = () => {
+  const [rockets, setRockets] = useState([]);
 
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/rockets')
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        setRockets(response);
+      });
+  }, []);
+  return (
+    <div>
+      {
+       rockets.map((rocket) => (
+         <div key={rocket.id}>
+           {rocket.rocke_name}
+         </div>
+       ))
+     }
+    </div>
+  );
+};
 export default Rockets;
