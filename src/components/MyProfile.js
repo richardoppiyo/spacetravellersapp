92f102dda1008235015ac2970540919e51b3de41
Richard Opiyo <romenyo@dewcis.com>
2022-04-06 19:51:53 +0300

    Implement filter for joined mission

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { displayMission } from '../redux/missions/missions';
 
-const MyProfile = () => (
-  <div>
-    <h1>My profile section</h1>
-  </div>
-);
+const Rockets = () => {
+  const missions = useSelector((state) => state.mission);
+  const dispatch = useDispatch();
 
-export default MyProfile;
+  const myMissions = missions !== null
+    ? missions.filter((item) => item.reserved !== false) || []
+    : [];
+
+  useEffect(() => {
+    dispatch(displayMission(missions));
+  }, []);
+
+  return (
+    <>
+      <div className="outerbox">
+        <div>
+          <h1>My mission</h1>
+          { myMissions.map((mission) => (
+            <div key={mission.mission_id}>
+              <ul>
+                <li>{mission.mission_name}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h1>My Rockets</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Rockets;
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Myprofile.css';
 
 const Rockets = () => (
   <div>
