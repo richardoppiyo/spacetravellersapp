b4e8095bfc5d3c49ec22a97617034a9dcf5abd92
Richard Opiyo <romenyo@dewcis.com>
2022-04-07 22:20:19 +0300

    add navbar heading

--- src/redux/missions/missionapi.js
+++ src/redux/missions/missionapi.js
@@ -5,19 +5,3 @@ const getMissions = async () => {
 };
 
 export default getMissions;
-// export const postBook = async (books) => {
-//   await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Mw1U4sRoKvmi9GEOKqfn/books/', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(books),
-//   });
-// };
-
-// export const deleteBook = async (id) => {
-//   await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Mw1U4sRoKvmi9GEOKqfn/books/${id}`,
-//     {
-//       method: 'DELETE',
-//     });
-// };
