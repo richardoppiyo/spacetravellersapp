4e7d7f4bb0da75ce99e2a55ba8ce6f3e04bc72fd
ivaan1992 <ivaan.lg.92@gmail.com>
2022-04-06 20:23:29 -0500

    Style the rockets and header section

--- src/components/rockets/Rocket.js
+++ src/components/rockets/Rocket.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RocketBtn from './RocketBtn';
+import './Rockets.css';
 
 const Rocket = ({
   image, name, description, id, reserved,
 }) => (
   <li className="Rocket">
-    <img className="Rocket__img" src={image} alt="rocket-img" />
-    <div className="Rocket__div">
-      <h1 className="Rocket__name">{name}</h1>
-      <div className="Rocket__badge_desc_div">
-        {reserved && <span className="Rocket__reserved_badge">Reserved</span>}
-        <p className="Rocket__description">{description}</p>
+    <div className="Rocket-Card">
+      <img className="Rocket__img" src={image} alt="rocket-img" />
+      <div className="Rocket__div">
+        <h1 className="Rocket__name">{name}</h1>
+        <div className="Rocket__badge_desc_div">
+          {reserved && <span className="Rocket__reserved_badge">Reserved</span>}
+          <p className="Rocket__description">{description}</p>
+        </div>
+        {reserved ? (
+          <RocketBtn
+            className="Rocket__btn_Cancel"
+            text="Cancel Reservation"
+            id={id}
+          />
+        ) : (
+          <RocketBtn
+            className="Rocket__btn_Reserve"
+            text="Reserve Rocket"
+            id={id}
+          />
+        )}
       </div>
-      {reserved ? (
-        <RocketBtn
-          className="Rocket__btn_Cancel"
-          text="Cancel Reservation"
-          id={id}
-        />
-      ) : (
-        <RocketBtn
-          className="Rocket__btn_Reserve"
-          text="Reserve Rocket"
-          id={id}
-        />
-      )}
     </div>
   </li>
 );
